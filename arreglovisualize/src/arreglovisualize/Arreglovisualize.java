61f86579816ecca12147b4e39614f8b4dc4d8adf
MonicaAguilar24 <145472036+MonicaAguilar24@users.noreply.github.com>
2023-09-27 07:49:46 -0600

    Add files via upload

---
+++ arreglovisualize/src/arreglovisualize/Arreglovisualize.java
@@ -0,0 +1,16 @@
+package arreglovisualize;
+import java.util.Scanner;
+public class Arreglovisualize {
+
+    public static void main(String[] args, Scanner Scanner) {
+   Scanner=new Scanner(System.in);
+        int p;
+   for(p=0;p<=10;p++)
+   {
+     System.out.print("Ingresa los numeros: ");
+     num[p]=leer nextFloat()}
+     for(p=0;p<=10;p++){
+       System.out.print("Visualizar"+num[p]);
+   }
+  }  
+}
